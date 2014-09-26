6693b9008a330aefd80729565e80d0e7d46cde12
Michael Daw <michael.k.daw@outlook.com>
2014-09-25 23:51:24 -0600

    stuff not working

--- public/app/login/services/facebookService.js
+++ public/app/login/services/facebookService.js
@@ -2,14 +2,14 @@ define(['./../module'], function (module) {
   'use strict';
   module.factory('facebookService', ['$window','docVarsService',
     function ($window,docVarsService) {
-      alert(docVarsService);
+      alert(docVarsService.FB_APP_ID);
       return{
         login: function(){
-//          $window.location.href =
-//            'https://www.facebook.com/dialog/oauth?' +
-//            'client_id=486331038173579' +
-//            '&redirect_uri=http://local.csart.herokuapp.com:3030/login-result/facebook' +
-//            '&response_type=token%20granted_scopes'
+          $window.location.href =
+            'https://www.facebook.com/dialog/oauth?' +
+            'client_id=' + docVarsService.FB_APP_ID
+            '&redirect_uri=http://local.csart.herokuapp.com:3030/login-result/facebook' +
+            '&response_type=token%20granted_scopes'
         }
       }
     }]);
--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ app.get('/app/*',function(req,res){
 
 app.get('*', function(req,res){
   res.render('index',{
-    title: "Skeletr"
+    title: "Skeletr",
+    FB_APP_ID: "10"
   });
 });
 
